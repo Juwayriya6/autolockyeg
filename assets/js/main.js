145b02b795608e6a948ac0056daca4637e213619
Juwayriya6 <juwayriyaart@gmail.com>
2025-10-20 18:57:50 -0600

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
+// Popup service form
+const serviceBtn = document.querySelectorAll('.service-btn');
 const popup = document.getElementById('service-popup');
-const serviceBtns = document.querySelectorAll('.service-btn');
-const closeBtn = document.querySelector('.close-btn');
+const closeBtn = document.querySelector('#service-popup .close-btn');
 
-serviceBtns.forEach(btn => {
+serviceBtn.forEach(btn => {
   btn.addEventListener('click', () => {
     popup.style.display = 'flex';
   });
@@ -12,6 +13,8 @@ closeBtn.addEventListener('click', () => {
   popup.style.display = 'none';
 });
 
-window.addEventListener('click', e => {
-  if (e.target === popup) popup.style.display = 'none';
+window.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    popup.style.display = 'none';
+  }
 });
